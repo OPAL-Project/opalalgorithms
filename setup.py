a6935fc57f965c11a871477136234705bcaee1a6
Shubham Jain <shubhamjain0594@gmail.com>
2018-03-19 14:43:39 +0000

    Modified package version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opalalgorithms',
-    version='0.1.1',
+    version='0.1.2',
     description='OPAL Algorithms. Package to implement algorithms to be run '
                 'on OPAL platform.',
     author='Shubham Jain, Axel Oehmichen, Yves-Alexandre De Montjoye',
